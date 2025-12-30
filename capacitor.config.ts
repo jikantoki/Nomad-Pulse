819e6e7743542c68612a42361a59873741de307c
ENOKI TOKI <tokisora123456@gmail.com>
2025-12-30 23:48:55 +0900

    ステータスバー問題の解消

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'xyz.enoki.nomadpulse',
   appName: 'NomadPulse',
   webDir: 'dist',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+    },
+  },
+  server: {
+    androidScheme: 'https',
+  },
 }
 
 export default config
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <template lang="pug">
 v-app
   v-main
+    .status-bar-padding(:class="isAndroid15OrHigher ? 'android-15-or-higher' : ''")
     router-view
 </template>
 
-<script lang="ts" setup>
-//
+<script lang="ts">
+  import { Device } from '@capacitor/device'
+  import { StatusBar } from '@capacitor/status-bar'
+
+  export default {
+    data () {
+      return {
+        isAndroid15OrHigher: false,
+      }
+    },
+    async mounted () {
+      StatusBar.setOverlaysWebView({
+        overlay: false,
+      })
+      const info = await Device.getInfo()
+      if (info.platform === 'android' && Number(info.osVersion) >= 15) {
+        this.isAndroid15OrHigher = true
+      }
+    },
+  }
 </script>
+
+<style lang="scss" scoped>
+.android-15-or-higher {
+  height: env(titlebar-area-height, 40px);
+}
+</style>
