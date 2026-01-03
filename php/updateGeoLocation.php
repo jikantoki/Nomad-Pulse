b13455f9a1ba558fdbfdf898d87f1e49eee6e502
ENOKI TOKI <tokisora123456@gmail.com>
2026-01-03 13:38:33 +0900

    add updateGeoLocation.php

---
+++ php/updateGeoLocation.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../env.php'; //環境変数読み込み
+require_once './settings.php'; //ルートディレクトリ読み込み
+require_once DIR_ROOT . '/php/myAutoLoad.php'; //自動読み込み
+require_once DIR_ROOT . '/php/functions/authAPIforUse.php'; //APIが有効かどうか自動判定
+require_once DIR_ROOT . '/php/functions/authAccountforUse.php'; //ログイン状態が有効かどうか判定
+
+if (
+  !isset($_SERVER['HTTP_ID'])
+) {
+  echo json_encode([
+    'status' => 'invalid',
+    'reason' => 'invalid authentication information',
+    'errCode' => 10
+  ]);
+  exit;
+}
+$id = $_SERVER['HTTP_ID'];
+$lat = $_SERVER['HTTP_LAT'];
+$lng = $_SERVER['HTTP_LNG'];
+$secretId = idToSecretId($id);
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+$unixtime = time();
+
+//既にデータが入っているか確認
+$res = SQLfind('location_list', 'secretId', $secretId);
+if (!$res) {
+  SQLinsert('location_list', [
+    'secretId' => $secretId,
+    'lat' => 0,
+    'lng' => 0,
+    'unixtime' => 0,
+    'ipAddress' => ''
+  ]);
+}
+SQL(`
+  update location_list set
+  lat = {$lat},
+  lng = {$lng},
+  unixtime = {$unixtime},
+  ipAddress = {$ipAddress}
+  where secretId = {$secretId}
+`);
+
+echo json_encode([
+  'status' => 'ok',
+  'id' => $id,
+  'unixtime' => $unixtime,
+]);
