72ee1c42d58d6b2ec1b7cb5e9298d4474b68145f
ENOKI TOKI <tokisora123456@gmail.com>
2026-01-12 16:24:41 +0900

    change gitignore

---
+++ php/getMyFriendList.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../env.php'; //環境変数読み込み
+require_once './settings.php'; //ルートディレクトリ読み込み
+require_once DIR_ROOT . '/php/myAutoLoad.php'; //自動読み込み
+require_once DIR_ROOT . '/php/functions/authAPIforUse.php'; //APIが有効かどうか自動判定
+require_once DIR_ROOT . '/php/functions/authAccountforUse.php'; //ログイン状態が有効かどうか判定
+
+if (
+  !isset($_SERVER['HTTP_ID'])
+) {
+  echo json_encode([
+    'status' => 'invalid',
+    'reason' => 'invalid authentication information',
+    'errCode' => 10
+  ]);
+  exit;
+}
+
+$id = $_SERVER['HTTP_ID'];
