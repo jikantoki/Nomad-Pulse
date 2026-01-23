fc32311e55a0c6df7c09e69c6e8aaf0739d02b2b
ENOKI TOKI <tokisora123456@gmail.com>
2026-01-23 23:27:54 +0900

    緊急修正

--- php/functions/mailFunctions.php
+++ php/functions/mailFunctions.php
@@ -1,10 +1,24 @@
 <?php
-require_once DIR_ROOT . '/env.php';
+// require_once DIR_ROOT . '/env.php';
 
 //メール関係
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
+// 本当はここenvから取得したい
+define('MailHeader', "<p>
+いつも Nomad Pulse をご利用いただきありがとうございます。
+<hr>
+</p>");
+define('MailFooter', "<p>
+<hr>
+このメールに返信することはできません。
+<br>
+また、このメールに身に覚えのない場合は、エノキ電気までお問い合わせください。
+<br>
+<a href=\"https://nomadpulse.enoki.xyz\">Nomad Pulse</a> by <a href=\"https://enoki.xyz\">エノキ電気</a>
+</p>");
 
 function sendMail($to, $title, $message)
 {
